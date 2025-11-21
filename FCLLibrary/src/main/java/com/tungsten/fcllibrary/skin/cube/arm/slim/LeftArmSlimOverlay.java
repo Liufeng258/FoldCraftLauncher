7afd63d7f4ad2f20f8c9f5445ef8cb65b3a698c4
Liufeng258 <160316559+Liufeng258@users.noreply.github.com>
2025-11-21 12:11:24 +0800

    Upload file to test(5)
    Just for testing.

--- FCLLibrary/src/main/java/com/tungsten/fcllibrary/skin/cube/arm/slim/LeftArmSlimOverlay.java
+++ FCLLibrary/src/main/java/com/tungsten/fcllibrary/skin/cube/arm/slim/LeftArmSlimOverlay.java
@@ -7,19 +7,20 @@ public class LeftArmSlimOverlay extends LimbCube {
     protected float[] leftArmSlimOverlayTexCoordinates;
     
     public LeftArmSlimOverlay(float scale) {
-        super(4.2352943f * scale, 12.705883f * scale, 4.2352943f * scale, 6.0f * scale, 2.0f * scale, 0.0f * scale,
+        super(3.1764708f * scale, 12.705883f * scale, 4.2352943f * scale, 5.5f * scale, 2.0f * scale, 0.0f * scale,
                 1.0f, 0.0f, 0.0f,
                 -0.5f, 10.0f, -10.0f,
                 0.333f, 20.0f, -20.0f);
+        float slimWidthRatio = 3.1764708f / 4.2352943f;
         this.leftArmSlimOverlayTexCoordinates = new float[] {
                 0.8125f, 1.0f, 0.8125f, 0.90625f, 0.875f, 0.90625f, 0.875f, 1.0f,
                 0.8125f, 0.90625f, 0.8125f, 0.8125f, 0.875f, 0.8125f, 0.875f, 0.90625f,
                 0.8125f, 0.8125f, 0.8125f, 0.75f, 0.875f, 0.75f, 0.875f, 0.8125f,
                 0.875f, 0.8125f, 0.875f, 0.75f, 0.9375f, 0.75f, 0.9375f, 0.8125f,
-                0.875f, 1.0f, 0.875f, 0.90625f, 0.9375f, 0.90625f, 0.9375f, 1.0f,
-                0.875f, 0.90625f, 0.875f, 0.8125f, 0.9375f, 0.8125f, 0.9375f, 0.90625f,
-                0.75f, 1.0f, 0.75f, 0.90625f, 0.8125f, 0.90625f, 0.8125f, 1.0f,
-                0.75f, 0.90625f, 0.75f, 0.8125f, 0.8125f, 0.8125f, 0.8125f, 0.90625f,
+                0.875f, 1.0f, 0.875f, 0.90625f, 0.875f + 0.0625f * slimWidthRatio, 0.90625f, 0.875f + 0.0625f * slimWidthRatio, 1.0f,
+                0.875f, 0.90625f, 0.875f, 0.8125f, 0.875f + 0.0625f * slimWidthRatio, 0.8125f, 0.875f + 0.0625f * slimWidthRatio, 0.90625f,
+                0.75f, 1.0f, 0.75f, 0.90625f, 0.75f + 0.0625f * slimWidthRatio, 0.90625f, 0.75f + 0.0625f * slimWidthRatio, 1.0f,
+                0.75f, 0.90625f, 0.75f, 0.8125f, 0.75f + 0.0625f * slimWidthRatio, 0.8125f, 0.75f + 0.0625f * slimWidthRatio, 0.90625f,
                 0.9375f, 1.0f, 0.9375f, 0.90625f, 1.0f, 0.90625f, 1.0f, 1.0f,
                 0.9375f, 0.90625f, 0.9375f, 0.8125f, 1.0f, 0.8125f, 1.0f, 0.90625f
         };
