c3a9b28a2924e0ceee05dbfd45c992f3e2f4d6e8
Liufeng258 <160316559+Liufeng258@users.noreply.github.com>
2025-11-24 14:13:38 +0800

    Refactor vertex definitions and clean comments
    Removed commented-out Chinese text and cleaned up vertex definitions.

--- FCLLibrary/src/main/java/com/tungsten/fcllibrary/skin/cube/LimbCubeSlim.java
+++ FCLLibrary/src/main/java/com/tungsten/fcllibrary/skin/cube/LimbCubeSlim.java
@@ -14,28 +14,22 @@ public class LimbCubeSlim extends LimbCube {
                        float subStepValue, float subMaxAngle, float subMinAngle) {
         super(n1, n2, n3, n4, n5, n6, n7, n8, n9, mainStepValue, mainMaxAngle, mainMinAngle, 
               subStepValue, subMaxAngle, subMinAngle);
-        
-        // 纤细手臂使用不同的顶点构造
-        // 重新定义顶点数组以适应纤细手臂的特殊几何结构
         this.vertices = new float[] {
-                // 纤细手臂的特殊顶点构造
-                -0.75f, -1.0f, 1.0f,    // 0
-                0.75f, -1.0f, 1.0f,     // 1
-                0.75f, 1.0f, 1.0f,      // 2
-                -0.75f, 1.0f, 1.0f,     // 3
+                -0.75f, -1.0f, 1.0f,
+                0.75f, -1.0f, 1.0f,
+                0.75f, 1.0f, 1.0f,
+                -0.75f, 1.0f, 1.0f,
 
-                -0.75f, -1.0f, -1.0f,   // 4
-                0.75f, -1.0f, -1.0f,    // 5
-                0.75f, 1.0f, -1.0f,     // 6
-                -0.75f, 1.0f, -1.0f,    // 7
+                -0.75f, -1.0f, -1.0f,
+                0.75f, -1.0f, -1.0f,
+                0.75f, 1.0f, -1.0f,
+                -0.75f, 1.0f, -1.0f,
 
-                -0.75f, 0.0f, 1.0f,     // 8
-                0.75f, 0.0f, 1.0f,      // 9
-                0.75f, 0.0f, -1.0f,     // 10
-                -0.75f, 0.0f, -1.0f     // 11
+                -0.75f, 0.0f, 1.0f,
+                0.75f, 0.0f, 1.0f,
+                0.75f, 0.0f, -1.0f,
+                -0.75f, 0.0f, -1.0f
         };
-        
-        // 重新计算面顶点
         for (int i = 0; i < this.faceIndecies.length; ++i) {
             final int n = this.faceIndecies[i];
             final float n2 = this.vertices[n * 3 + 1] * this.scale[1] / 2.0f;
